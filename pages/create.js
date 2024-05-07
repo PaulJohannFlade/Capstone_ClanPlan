f110a38b6b933b433d64212fbe6649e9264d54ea
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-05-07 21:44:14 +0200

    push notifications implemented

--- pages/create.js
+++ pages/create.js
@@ -1,18 +1,26 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 export default function CreatePage({ categories, familyMembers }) {
   const router = useRouter();
 
   async function handleAddTask(newTaskData) {
-    const response = await fetch("/api/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTaskData),
-    });
+    const response = await toast.promise(
+      fetch("/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTaskData),
+      }),
+      {
+        pending: "Task adding is pending",
+        success: "Task added successfully",
+        error: "Task not added",
+      }
+    );
     if (response.ok) {
       router.push("/");
     }
