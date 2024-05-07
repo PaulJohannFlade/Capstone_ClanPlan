65889d350baf3dd089b040f23f5b997f5693ec62
Mariia Posselt <maruw@ya.ru>
2024-05-07 14:30:01 +0200

    style toggle-input

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,49 +1,66 @@
 import styled from "styled-components";
+import Sun from "@/public/assets/images/sun.svg";
+import Moon from "@/public/assets/images/moon.svg";
 
-const ToggleSwitch = styled.div`
+const ToggleLabel = styled.label`
+  position: absolute;
+  top: 20px;
+  right: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const ToggleLabel = styled.label`
-  display: inline-block;
-  width: 50px;
-  height: 24px;
+const ToggleCheckbox = styled.input`
+  appearance: none;
+  width: 60px;
+  height: 30px;
   background-color: var(--color-font);
-  /* background-color: ${({ $dark }) => ($dark ? "#333" : "#ccc")}; */
-  border-radius: 12px;
-  position: relative;
-  cursor: pointer;
+  border-radius: 20px;
   transition: background-color 0.3s;
+  cursor: pointer;
 
   &:before {
     content: "";
-    font-size: 16px;
+    border: 0.5px solid var(--color-font);
     position: absolute;
-    top: 4px;
-    left: ${({ $dark }) => ($dark ? "20px" : "calc(100% - 20px)")};
-    transform: translateX(${({ $dark }) => ($dark ? "0" : "-100%")});
-    transition: transform 0.3s;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: var(--color-background);
+    transition: transform 0.3s ease;
+    transform: ${({ checked }) =>
+      checked ? "translateX(30px)" : "translateX(0)"};
   }
 `;
 
-const ToggleCheckbox = styled.input`
-  display: none;
+const SunIcon = styled(Sun)`
+  color: #344648;
+  fill: #344648;
+  position: absolute;
+  top: 3px;
+  left: 3px;
+`;
+
+const MoonIcon = styled(Moon)`
+  color: #ffffff;
+  fill: #ffffff;
+  position: absolute;
+  top: 6px;
+  right: 6px;
 `;
 
 export default function ThemeToggle({ isDarkTheme, onToggleTheme }) {
   return (
-    <ToggleSwitch>
+    <ToggleLabel htmlFor="theme-toggle" $dark={isDarkTheme}>
       <ToggleCheckbox
         type="checkbox"
         id="theme-toggle"
         onChange={onToggleTheme}
         checked={isDarkTheme}
       />
-      <ToggleLabel htmlFor="theme-toggle" $dark={isDarkTheme}>
-        {isDarkTheme ? "☀︎" : "☾"}
-      </ToggleLabel>
-    </ToggleSwitch>
+      <SunIcon />
+      <MoonIcon />
+    </ToggleLabel>
   );
 }
