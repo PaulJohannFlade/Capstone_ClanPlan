a3a895b7d30bc40bf936018b18ce95974d3a4bdd
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-05-07 15:30:19 +0200

    utility function

--- utils/checkForMissedDate.js
+++ utils/checkForMissedDate.js
@@ -3,6 +3,5 @@ export default function checkForMissedDate(dueDate) {
   const isMissed =
     new Date(dueDate).toISOString().substring(0, 10) <
     today.toISOString().substring(0, 10);
-
   return isMissed;
 }
