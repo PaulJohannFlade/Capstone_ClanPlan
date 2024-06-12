863db44d483071aaaceb7c6a273b62c726ce3fb3
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-06-12 15:37:00 +0200

    edit family name implemented

--- pages/_app.js
+++ pages/_app.js
@@ -39,6 +39,7 @@ export default function App({
       setListType("today");
     }
     setMyTasksSelection(!myTasksSelection);
+    setFilters({});
   }
 
   return (
--- pages/family/index.js
+++ pages/family/index.js
@@ -6,29 +6,99 @@ import Modal from "@/components/Modal";
 import { toast } from "react-toastify";
 import StyledPlus from "@/components/StyledPlus";
 import emailjs from "@emailjs/browser";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useModal } from "@/context/modalContext";
 import { useData } from "@/context/dataContext";
+import StyledPen from "@/components/StyledPen";
+import FamilyRegisterForm from "@/components/FamilyRegisterForm";
 
 const StyledMenu = styled.menu`
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 3fr 1fr;
   align-items: center;
   justify-content: center;
   @media (min-width: 900px) {
-    grid-template-columns: 1fr 1.7fr;
+    grid-template-columns: 1fr 1.7fr 1fr;
   }
 `;
 
 const StyledHeading = styled.h2`
-  text-align: left;
+  text-align: center;
+  box-shadow: 1px 0px 14px 10px #e8e8e8 inset;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  @media (max-width: 400px) {
+    max-width: 230px;
+  }
+
+  @media (min-width: 400px) {
+    max-width: 300px;
+  }
+  @media (min-width: 500px) {
+    max-width: 350px;
+  }
+
+  @media (min-width: 600px) {
+    max-width: 400px;
+  }
+  @media (min-width: 700px) {
+    max-width: 450px;
+  }
+  @media (min-width: 800px) {
+    max-width: 500px;
+  }
+  @media (min-width: 900px) {
+    max-width: 100%;
+  }
+`;
+
+const StyledPenUpdated = styled(StyledPen)`
+  right: -0.75rem;
+  top: -0.5rem;
+`;
+
+const StyledContainer = styled.div`
+  position: relative;
 `;
 
 export default function FamilyPage() {
-  const { familyMembers, mutateMembers, user } = useData();
+  const { familyMembers, mutateMembers, user, mutateUser } = useData();
   const form = useRef();
   const { showModal, openModal, closeModal } = useModal();
+  const [modalMode, setModalMode] = useState("");
 
+  function handleAddFamilyMember() {
+    openModal();
+    setModalMode("add-member");
+  }
+
+  function handleEditFamilyName() {
+    openModal();
+    setModalMode("edit-family");
+  }
+
+  async function handleUpdateFamilyName(updatedFamilyName) {
+    const response = await toast.promise(
+      fetch("/api/family", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updatedFamilyName),
+      }),
+      {
+        pending: "Family Name updation is pending",
+        success: "Family Name updated successfully",
+        error: "Family Name not updated",
+      }
+    );
+    if (response.ok) {
+      closeModal();
+      mutateUser();
+    }
+  }
   async function handleAddMember(newMemberData) {
     const response = await toast.promise(
       fetch("/api/members", {
@@ -67,22 +137,35 @@ export default function FamilyPage() {
   return (
     <>
       <StyledMenu>
-        <StyledPlus onClick={openModal} $right={true} />
-        <StyledHeading>Family: {user?.family?.name}</StyledHeading>
+        <StyledPlus onClick={handleAddFamilyMember} $right={true} />
+        <StyledContainer>
+          <StyledHeading>Family: {user?.family?.name}</StyledHeading>
+          <StyledPenUpdated onClick={handleEditFamilyName} />
+        </StyledContainer>
       </StyledMenu>
       {!familyMembers.length && (
         <StyledMessage>The list is empty. Add members to begin!</StyledMessage>
       )}
       <FamilyMembersList familyMembers={familyMembers} />
 
-      <Modal $top="7rem" $open={showModal}>
-        {showModal && (
+      <Modal $top="7rem" $open={modalMode === "add-member" && showModal}>
+        {modalMode === "add-member" && showModal && (
           <MemberForm
             onAddMember={handleAddMember}
             familyMembers={familyMembers}
             user={user}
             form={form}
             heading={"Add new family member"}
+          />
+        )}
+      </Modal>
+      <Modal $top="7rem" $open={modalMode === "edit-family" && showModal}>
+        {modalMode === "edit-family" && showModal && (
+          <FamilyRegisterForm
+            onAddFamily={handleUpdateFamilyName}
+            value={user?.family}
+            isEdit
+            heading={"Edit family name"}
           />
         )}
       </Modal>
