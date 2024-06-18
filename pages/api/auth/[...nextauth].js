212cf1f3c3cc3a5a37ab19ff7195e49a07bb5a05
Paul Johann Flade <paul-johann@flade-privat.de>
2024-06-18 13:04:01 +0200

    add google as provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -42,6 +42,28 @@ export const authOptions = {
           clientId: process.env.GITHUB_ID,
           clientSecret: process.env.GITHUB_SECRET,
         }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+      callbacks: {
+        async signIn({ account, profile }) {
+          if (account.provider === "google") {
+            return (
+              profile.email_verified && profile.email.endsWith("@example.com")
+            );
+          }
+          return true; // Do different verification for other providers that don't have `email_verified`
+        },
+      },
+    }),
+
     // ...add more providers here
   ],
 };
