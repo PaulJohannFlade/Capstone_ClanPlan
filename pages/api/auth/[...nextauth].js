1cf5310cc5c86d41a5b8464df550676eb1554b0f
Paul Johann Flade <paul-johann@flade-privat.de>
2024-06-20 11:01:28 +0200

    Revert "Feature/add provider"

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -39,31 +38,11 @@ export const authOptions = {
             }
           },
         })
-      : (GithubProvider({
+      : GithubProvider({
           clientId: process.env.GITHUB_ID,
           clientSecret: process.env.GITHUB_SECRET,
         }),
-        GoogleProvider({
-          clientId: process.env.GOOGLE_ID,
-          clientSecret: process.env.GOOGLE_SECRET,
-          authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code",
-            },
-          },
-        })),
-
     // ...add more providers here
   ],
-  callbacks: {
-    async signIn({ account, profile }) {
-      if (account.provider === "google") {
-        return profile.email_verified;
-      }
-      return true; // Do different verification for other providers that don't have `email_verified`
-    },
-  },
 };
 export default NextAuth(authOptions);
