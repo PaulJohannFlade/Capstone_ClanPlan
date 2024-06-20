f9e14be60cbe5393455179ca3301435f04788fa3
Paul Johann Flade <paul-johann@flade-privat.de>
2024-06-20 12:26:00 +0200

    refine Tenory

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,58 +5,62 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
-  providers: [
+  providers:
     process.env.VERCEL_ENV === "preview"
-      ? CredentialsProvider({
-          name: "credentials",
-          credentials: {
-            username: {
-              label: "Username",
-              type: "text",
-              placeholder: "username",
+      ? [
+          CredentialsProvider({
+            name: "credentials",
+            credentials: {
+              username: {
+                label: "Username",
+                type: "text",
+                placeholder: "username",
+              },
+              password: { label: "Password", type: "password" },
             },
-            password: { label: "Password", type: "password" },
-          },
-          async authorize(credentials) {
-            if (
-              credentials.username === "turtle" &&
-              credentials.password === "turtle"
-            ) {
-              return {
-                name: "New Turtle",
-                email: "test@example.com",
-              };
-            } else if (
-              credentials.username === "clanplan" &&
-              credentials.password === "clanplan"
-            ) {
-              return {
-                name: "New User",
-                email: "newUser@clanPlan.com",
-              };
-            } else {
-              return null;
-            }
-          },
-        })
-      : (GithubProvider({
-          clientId: process.env.GITHUB_ID,
-          clientSecret: process.env.GITHUB_SECRET,
-        }),
-        GoogleProvider({
-          clientId: process.env.GOOGLE_ID,
-          clientSecret: process.env.GOOGLE_SECRET,
-          authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code",
+            async authorize(credentials) {
+              if (
+                credentials.username === "turtle" &&
+                credentials.password === "turtle"
+              ) {
+                return {
+                  name: "New Turtle",
+                  email: "test@example.com",
+                };
+              } else if (
+                credentials.username === "clanplan" &&
+                credentials.password === "clanplan"
+              ) {
+                return {
+                  name: "New User",
+                  email: "newUser@clanPlan.com",
+                };
+              } else {
+                return null;
+              }
             },
-          },
-        })),
+          }),
+        ]
+      : [
+          GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+          }),
+          GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            authorization: {
+              params: {
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code",
+              },
+            },
+          }),
+        ],
 
-    // ...add more providers here
-  ],
+  // ...add more providers here
+
   callbacks: {
     async signIn({ account, profile }) {
       if (account.provider === "google") {
